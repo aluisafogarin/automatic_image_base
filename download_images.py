e146ce791e3bd530c860e9d11ea5cb3369724329
Ana Luísa <ana.luisa.fogarin@hotmail.com>
2019-11-27 00:20:40 -0300

    Downloading every data received

--- download_images.py
+++ download_images.py
@@ -5,6 +5,7 @@
     https://docs.sunpy.org/projects/drms/en/latest/tutorial.html
 """
 
+import sys 
 import drms 
 import os 
 import csv
@@ -30,58 +31,72 @@ r = c.export('hmi.Ic_*[2014-02-25_00:49_TAI]')
 r.urls.url[0]
 r.download(out_dir, 0)
 """
-#Create the destiny folders when necessary 
-continuum = 'continuum'
-if not os.path.exists(continuum):
-    os.mkdir(continuum)
-
-aia1600 = 'aia1600'
-if not os.path.exists(aia1600):
-    os.mkdir(aia1600)
-
-aia1700 = 'aia1700'
-if not os.path.exists(aia1700):
-    os.mkdir(aia1700)
+def downloadImages(): 
+    #Create the destiny folders when necessary 
+    continuum = 'continuum'
+    if not os.path.exists(continuum):
+        os.mkdir(continuum)
     
-file = csv.DictReader(open("validDate.csv","r"))
-for file in file :
-    listDate = file['Year']
-    listTime = file['Max']
-    listTime = listTime[:-3]
+    aia1600 = 'aia1600'
+    if not os.path.exists(aia1600):
+        os.mkdir(aia1600)
     
-#Downloading images on HMI Continuum 
+    aia1700 = 'aia1700'
+    if not os.path.exists(aia1700):
+        os.mkdir(aia1700)
+        
+    inputFile = csv.DictReader(open(validDataFile,"r"))
+    for row in inputFile :
+        listDate = row['Year']
+        listTime = row['Max']
+        listTime = listTime[:-3]
+   
+        #Downloading images on HMI Continuum 
+        
+        #hmi.ic_45s.20120307_002400_TAI.2.continuum.fits 
+        #hmi.ic+45s.YYYYMMDD_HHMMSS_TAI.2.CONTINUUM.fits
+        #c.series(r'hmi.Ic_*') To know coontinuum types of images available 
+        dc = 'hmi.Ic_45s['+listDate +'_'+listTime+'_TAI/30m@30m]'
+        dc = dc.replace(" ", "") #Removes blank spaces
+        
+        r = c.export(dc, method='url', protocol='fits')  #Using url/fits 
+        r.wait()
+        r.status
+        r.request_url
+        r.download(continuum)
+        
+        #Downloading images on AIA 1600 and 1700 
+        #/30m@3m - gets a image once in every 3 minutes 
+        #ds = 'aia.lev1_uv_24s[2014-02-25_00:49/30m@3m][1600,1700]' Encontra 20 arquivos 
+        
+        #aia.lev1_uv_24s.2012-03-07T002355Z.1600.image_lev1
+        #aia.lev1_uv_24s.YYYY-MM-DDTHHMMSSZ.1600.image_lev1.fits
+        da = 'aia.lev1_uv_24s['+listDate+'_'+listTime+'/30m@30m][1600]'
+        r = c.export(da, method='url', protocol='fits')
+        r.wait()
+        r.status
+        r.request_url
+        r.download(aia1600)
+        
+        #aia.lev1_uv_24s.YYYY-MM.DDTHHMMSSZ.1700.image_lev1.fits 
+        #aia.lev1_uv_24s.2012-03-07T002408Z.1700.image_lev1
+        daia = 'aia.lev1_uv_24s['+listDate+'_'+listTime+'/30m@30m][1700]'
+        r = c.export(daia, method='url', protocol='fits')
+        r.wait()
+        r.status
+        r.request_url
+        r.download(aia1700)
+        
+    inputFile.close
     
-#c.series(r'hmi.Ic_*') To know coontinuum types of images available 
-dc = 'hmi.Ic_45s['+listDate +'_'+listTime+'_TAI/30m@3m]'
-dc = dc.replace(" ", "") #Removes blank spaces
-#print(ds)
-
-#Using url/fits 
-r = c.export(dc, method='url', protocol='fits')    
-r.wait()
-r.status
-r.request_url
-r.download(continuum)
-
-#Downloading images on AIA 1600 and 1700 
-
-#/30m - pelos próximos 30 minutos @3m - uma vez a cada 3 minutos 
-#ds = 'aia.lev1_uv_24s[2014-02-25_00:49/30m@3m][1600,1700]' Encontra 20 arquivos 
-
-da = 'aia.lev1_uv_24s['+listDate+'_'+listTime+'/30m@3m][1600]'
-r = c.export(da, method='url', protocol='fits')
-r.wait()
-r.status
-r.request_url
-r.download(aia1600)
-
-daia = 'aia.lev1_uv_24s['+listDate+'_'+listTime+'/30m@3m][1700]'
-r = c.export(daia, method='url', protocol='fits')
-r.wait()
-r.status
-r.request_url
-r.download(aia1700)
-
+try:
+    validDataFile = sys.argv[1]
+    
+    downloadImages()
+    
+except IndexError:
+    print("Incorrect parameters")
+    print("Try: >$ python download_images.py <file_with_correct_flares>")
 
 
 
